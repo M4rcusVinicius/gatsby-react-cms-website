c04b00d55336e9f84ce2c54b6d29a42ad92c5984
Marcus Vinicius <marcus.vinicius.mvap@gmail.com>
2020-12-15 12:08:08 -0300

    Navbar

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import GlobalStyles from "../../styles/global"
 import * as S from "./styled"
+import Navbar from '../Navbar'
 
 const Layout = ({ children }) => (
     <S.LayoutWrapper>
       <GlobalStyles />
+      <Navbar />
       <S.LayoutMain />
       <main>{children}</main>
     </S.LayoutWrapper>
