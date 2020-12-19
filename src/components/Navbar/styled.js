345847cf6fe7ee61a9791dd16254749ada923fec
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2020-12-19 15:10:45 -0300

    Dark mode colors

--- src/components/Navbar/styled.js
+++ src/components/Navbar/styled.js
@@ -4,4 +4,7 @@ export const NavbarWrapper = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  background-color: var(--secondaryBackground);
+  padding: 1rem;
+  margin-bottom: 2rem;
 `
--- src/components/layout/styled.js
+++ src/components/layout/styled.js
@@ -2,6 +2,7 @@
 import styled from "styled-components"
 
 export const LayoutWrapper = styled.section`
+  background-color: var(--primaryBackground);
 `
 
 export const LayoutMain = styled.main`
