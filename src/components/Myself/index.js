42f35023bf475ef73f9cbb56bde6673fe427441a
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2020-12-29 22:19:48 -0300

    Profile

--- src/components/Myself/index.js
+++ src/components/Myself/index.js
@@ -1,36 +1,19 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import LogoImage from '../LogoImage'
+import MyPhoto from './MyPhoto'
 
 import * as S from './styled'
 
-const Logo = () => {
-  const {
-    site: {
-      siteMetadata: { title, company },
-    },
-  } = useStaticQuery(graphql`
-    query MySiteMetadata {
-      site {
-        siteMetadata {
-          title
-          company
-        }
-      }
-    }
-  `)
-
-  return (
-    <S.LogoWrapper>
-      <S.LogoLink to="/">
-        <LogoImage />
-        <S.LogoTextContent>
-          <S.LogoText>{title}</S.LogoText>
-          <S.LogoCompany>Por {company}</S.LogoCompany>
-        </S.LogoTextContent>
-      </S.LogoLink>
-    </S.LogoWrapper>
+const Myself = () => (
+    <S.MyselfWrapper>
+      <S.MyselfLink to="https://www.linkedin.com/in/marcus-vinicius-almeida-pires-b194691b6/">
+        <MyPhoto />
+        <S.MyselfTextContent>
+          <S.MyselfText>Marcus Vinicius</S.MyselfText>
+          <S.MyselfCompany>Full stack developer</S.MyselfCompany>
+        </S.MyselfTextContent>
+      </S.MyselfLink>
+    </S.MyselfWrapper>
   )
-}
 
-export default Logo
+export default Myself
--- src/components/Myself/styled.js
+++ src/components/Myself/styled.js
@@ -1,26 +1,26 @@
 import styled from 'styled-components'
 import { Link } from "gatsby"
 
-export const LogoWrapper = styled.div`
+export const MyselfWrapper = styled.div`
 `
 
-export const LogoLink = styled(Link)`
+export const MyselfLink = styled(Link)`
   text-decoration: none;
   display: flex;
   align-items: center;
 `
 
-export const LogoTextContent = styled.div`
+export const MyselfTextContent = styled.div`
  margin-left: 0.4rem
 `
 
-export const LogoText = styled.h1`
+export const MyselfText = styled.h1`
   font-weight: 700;
   font-size: 1.2rem;
   color: var(--blueDark);
 `
 
-export const LogoCompany = styled.h3`
+export const MyselfCompany = styled.h3`
   font-weight:600;
   font-size: 1rem;
   margin-top: 0.2rem;
